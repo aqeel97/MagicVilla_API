02257936ae9eb2143dabd5d9c0388664f796858c
Aqeel Jabbar <Aqeel Jabbar@DESKTOP-FBOTMVV>
2023-12-19 04:01:42 +0300

    dbcontext

---
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace MagicVilla_VillaAPI.Data
+{
+    //in Consol write this commands to migration the Database
+    //add-migration AddVillaTable
+    //to apply this migration add write this command
+    //update-database
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        }
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,12 +1,18 @@
+using MagicVilla_VillaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 builder.Services.AddControllers(option =>
 {
     //option.ReturnHttpNotAcceptable=true;//to not return any values just Json in API
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();//add newtonsoftjson here <- || thelast controller to read XML value in response 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn m  ore about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
